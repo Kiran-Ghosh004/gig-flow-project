92da898cb4893698137134e0bdcdfa35b82ba996
Kiran-Ghosh004 <kiranghosh520@gmail.com>
2026-01-14 00:53:56 +0530

    finall commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,15 +5,16 @@ import authRoutes from "./routes/authRoutes.js";
 import gigRoutes from "./routes/gigRoutes.js";
 import bidRoutes from "./routes/bidRoutes.js";
 
-
 const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 // Routes
@@ -21,15 +22,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/gigs", gigRoutes);
 app.use("/api/bids", bidRoutes);
 
-// Test route
+// Health check
 app.get("/api/health", (req, res) => {
   res.status(200).json({
     status: "ok",
-    message: "GigFlow backend running"
+    message: "GigFlow backend running",
   });
 });
-
-
-
 
 export default app;
